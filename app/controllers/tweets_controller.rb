3112c0d1db7f41de85d35838589feca03cd2e2fc
Anthony <alasam@live.com>
2022-02-06 06:49:25 -0500

    add tweets controller, add show and create method in tweets controller, create tweets table and post colomn, create simple post and show erbs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,22 @@
 class TweetsController < ApplicationController
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+   
+    @tweet.save
+    redirect_to @tweet
+  end
+   
+  private
+    def tweet_params
+      params.require(:tweet).permit(:post)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_204605) do
+ActiveRecord::Schema.define(version: 2022_02_06_114004) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "post"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
