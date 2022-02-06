09662d6aba98b8ad67bb1bdbca16ca5569f4b9c6
Anthony <alasam@live.com>
2022-02-06 12:26:50 -0500

    complete edit functionality

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,17 +10,35 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   before_action :authorize
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
   def create
     @tweet = Tweet.new(tweet_params)
    
-    @tweet.save
-    redirect_to @tweet
+    if @tweet.save
+      redirect_to '/tweets'
+    else
+      render "new"
+    end
   end
    
+  def update
+    @tweet = Tweet.find(params[:id])
+   
+    if @tweet.update(tweet_params)
+      redirect_to '/tweets'
+    else
+      render "edit"
+    end
+  end
+
   private
     def tweet_params
       params.require(:tweet).permit(:post)
